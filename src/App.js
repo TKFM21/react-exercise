c1232e389bdb78a9110639e1b6f904b22081ea49
Fumiaki Takizawa <tkzwfm21@gmail.com>
2019-11-03 21:52:01 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world.</h1>
-    </div>
-  );
+const titleStyle = {
+  backgroundColor: 'red'
+}
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: 'test-state1'
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 style={titleStyle}>Hello world.</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
