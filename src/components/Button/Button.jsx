2f4058d16d1d5a79dcbf2f4d34f5b3e70d3b51ed
Fumiaki Takizawa <tkzwfm21@gmail.com>
2019-11-03 23:41:56 +0900

    function and class compornent exercise2

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import './Button.css';
 
 class Button extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
-
     render() {
         return (
             <span className="Button-container">
